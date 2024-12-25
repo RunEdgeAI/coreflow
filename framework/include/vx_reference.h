35a1095666eae112baeda28786279447b482a7a7
Andrew Mikhail <andrewmikhail@ymail.com>
2024-12-24 22:20:04 -0800

    Additional changes for successful build on other platforms

--- framework/include/vx_reference.h
+++ framework/include/vx_reference.h
@@ -42,7 +42,7 @@ public:
      * @brief Destroy the vx reference object
      * @ingroup group_int_reference
      */
-    ~Reference();
+    virtual ~Reference();
 
     /*! \brief Used to create a reference.
      * \note This does not add the reference to the system context yet.
