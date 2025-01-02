c18457fa0cadbdc38fe3f0bbe059b9c765193caa
Andrew Mikhail <andrewmikhail@ymail.com>
2025-01-02 15:44:49 -0800

    Dependabot ci & Test VX Conformance fix (#5)
    * Added dependabot for updating github actions and enabled ASAN in CI
    
    * Moving to correct directory
    
    * Unique name for asan workflow
    
    * Disabling ASAN in CI for now
    
    * Fixing conformance test script

--- test_vx_conformance.sh
+++ test_vx_conformance.sh
@@ -1,32 +1,38 @@
 #! /bin/bash
+set -e
 
-mkdir -p cts/build
+TARGET_DIR="${GITHUB_WORKSPACE:-$PWD}"
+echo "Running from TARGET_DIR=$TARGET_DIR"
+
+mkdir -p $TARGET_DIR/cts/build
 
 # Set the environment variables
-export OPENVX_DIR=$PWD
+export OPENVX_DIR=$TARGET_DIR
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$OPENVX_DIR/bazel-bin:$OPENVX_DIR/bazel-bin/vxu:$OPENVX_DIR/bazel-bin/targets/c_model:$OPENVX_DIR/bazel-bin/targets/extras:$OPENVX_DIR/bazel-bin/targets/debug:$OPENVX_DIR/bazel-bin/targets/opencl:$OPENVX_DIR/cts/build/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OPENVX_DIR/bazel-bin:$OPENVX_DIR/bazel-bin/vxu:$OPENVX_DIR/bazel-bin/targets/c_model:$OPENVX_DIR/bazel-bin/targets/extras:$OPENVX_DIR/bazel-bin/targets/debug:$OPENVX_DIR/bazel-bin/targets/opencl:$OPENVX_DIR/cts/build/lib/
-export VX_TEST_DATA_PATH=$HOME/Projects/OpenVX-sample-impl/cts/test_data/
+export VX_TEST_DATA_PATH=$OPENVX_DIR/cts/test_data/
 export VX_CL_SOURCE_DIR=$OPENVX_DIR/kernels/opencl/
 
+# Set the correct library extension based on the OS
+if [[ "$(uname)" == "Darwin" ]]; then
+  EXT=".dylib"
+else
+  EXT=".so"
+fi
+
 # Build the conformance test suite
-cd cts/build
+cd $TARGET_DIR/cts/build
 cmake \
 -DCMAKE_BUILD_TYPE=Debug \
 -DOPENVX_INCLUDES=$OPENVX_DIR/include \
--DOPENVX_LIBRARIES=$OPENVX_DIR/bazel-bin/libopenvx.dylib\;$OPENVX_DIR/bazel-bin/vxu/libvxu.dylib\;pthread\;dl\;m \
+-DOPENVX_LIBRARIES="$OPENVX_DIR/bazel-bin/libopenvx$EXT"\;"$OPENVX_DIR/bazel-bin/vxu/libvxu$EXT"\;pthread\;dl\;m \
 -DOPENVX_USE_USER_DATA_OBJECT=ON \
 -DOPENVX_USE_IX=ON \
 -DOPENVX_USE_U1=ON \
--DOPENVX_USE_S16=ON \
 -DOPENVX_USE_NN=ON \
 -DOPENVX_USE_NN_16=ON \
--DOPENVX_USE_OPENCL_INTEROP=ON \
 -DOPENVX_CONFORMANCE_NNEF_IMPORT=ON \
 -DOPENVX_CONFORMANCE_NEURAL_NETWORKS=ON \
--DEXPERIMENTAL_USE_DOT=ON \
--DEXPERIMENTAL_USE_OPENCL=ON \
--DEXPERIMENTAL_PLATFORM_SUPPORTS_16_FLOAT=ON \
 ..
 
 # -DOPENVX_CONFORMANCE_VISION=ON \
@@ -40,5 +46,5 @@ cmake --build .
 ./bin/vx_test_conformance
 
 # Clean up
-cd ../../
-rm -rf cts/build
+cd $TARGET_DIR
+rm -rf $TARGET_DIR/cts/build
