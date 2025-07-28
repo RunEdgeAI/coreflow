23f352c04f8866370a8df641a612f59f4e53737b
Andrew Mikhail <andrewmikhail@ymail.com>
2025-07-27 17:52:38 -0700

    More minor cleanup to type pairs

--- framework/src/vx_type_pairs.cpp
+++ framework/src/vx_type_pairs.cpp
@@ -13,57 +13,7 @@
  * limitations under the License.
  */
 
-#include "vx_internal.h"
 #include "vx_type_pairs.h"
-
-const vx_enum_string_t type_pairs[] = {
-    {VX_STRINGERIZE(VX_TYPE_INVALID), 0},
-    /* scalar objects */
-    {VX_STRINGERIZE(VX_TYPE_CHAR), sizeof(vx_char) * 2},
-    {VX_STRINGERIZE(VX_TYPE_UINT8), sizeof(vx_uint8) * 2},
-    {VX_STRINGERIZE(VX_TYPE_UINT16), sizeof(vx_uint16) * 2},
-    {VX_STRINGERIZE(VX_TYPE_UINT32), sizeof(vx_uint32) * 2},
-    {VX_STRINGERIZE(VX_TYPE_UINT64), sizeof(vx_uint64) * 2},
-    {VX_STRINGERIZE(VX_TYPE_INT8), sizeof(vx_int8) * 2},
-    {VX_STRINGERIZE(VX_TYPE_INT16), sizeof(vx_int16) * 2},
-    {VX_STRINGERIZE(VX_TYPE_INT32), sizeof(vx_int32) * 2},
-    {VX_STRINGERIZE(VX_TYPE_INT64), sizeof(vx_int64) * 2},
-    {VX_STRINGERIZE(VX_TYPE_FLOAT32), sizeof(vx_float32) * 2},
-    {VX_STRINGERIZE(VX_TYPE_FLOAT64), sizeof(vx_float64) * 2},
-    {VX_STRINGERIZE(VX_TYPE_SIZE), sizeof(vx_size) * 2},
-    {VX_STRINGERIZE(VX_TYPE_DF_IMAGE), sizeof(vx_df_image) * 2},
-    {VX_STRINGERIZE(VX_TYPE_BOOL), sizeof(vx_bool) * 2},
-    {VX_STRINGERIZE(VX_TYPE_ENUM), sizeof(vx_enum) * 2},
-    /* struct objects */
-    {VX_STRINGERIZE(VX_TYPE_COORDINATES2D), sizeof(vx_coordinates2d_t) * 2},
-    {VX_STRINGERIZE(VX_TYPE_COORDINATES3D), sizeof(vx_coordinates3d_t) * 2},
-    {VX_STRINGERIZE(VX_TYPE_RECTANGLE), sizeof(vx_rectangle_t) * 2},
-    {VX_STRINGERIZE(VX_TYPE_KEYPOINT), sizeof(vx_keypoint_t) * 2},
-    /* data objects */
-    {VX_STRINGERIZE(VX_TYPE_ARRAY), sizeof(Array)},
-    {VX_STRINGERIZE(VX_TYPE_DISTRIBUTION), sizeof(Distribution)},
-    {VX_STRINGERIZE(VX_TYPE_LUT), sizeof(Lut)},
-    {VX_STRINGERIZE(VX_TYPE_IMAGE), sizeof(Image)},
-    {VX_STRINGERIZE(VX_TYPE_CONVOLUTION), sizeof(Convolution)},
-    {VX_STRINGERIZE(VX_TYPE_THRESHOLD), sizeof(Threshold)},
-    {VX_STRINGERIZE(VX_TYPE_TENSOR), sizeof(Tensor)},
-    {VX_STRINGERIZE(VX_TYPE_MATRIX), sizeof(Matrix)},
-    {VX_STRINGERIZE(VX_TYPE_OBJECT_ARRAY), sizeof(ObjectArray)},
-    {VX_STRINGERIZE(VX_TYPE_SCALAR), sizeof(Scalar)},
-    {VX_STRINGERIZE(VX_TYPE_PYRAMID), sizeof(Pyramid)},
-    {VX_STRINGERIZE(VX_TYPE_REMAP), sizeof(Remap)},
-#ifdef OPENVX_KHR_XML
-    {VX_STRINGERIZE(VX_TYPE_IMPORT), sizeof(Import)},
-#endif
-    /* framework objects */
-    {VX_STRINGERIZE(VX_TYPE_CONTEXT), sizeof(Context)},
-    {VX_STRINGERIZE(VX_TYPE_GRAPH), sizeof(Graph)},
-    {VX_STRINGERIZE(VX_TYPE_NODE), sizeof(Node)},
-    {VX_STRINGERIZE(VX_TYPE_KERNEL), sizeof(Kernel)},
-    {VX_STRINGERIZE(VX_TYPE_TARGET), sizeof(Target)},
-    {VX_STRINGERIZE(VX_TYPE_REFERENCE), sizeof(Reference)},
-    {VX_STRINGERIZE(VX_TYPE_PARAMETER), sizeof(Parameter)},
-};
 
 #if defined(EXPERIMENTAL_USE_DOT) || defined(OPENVX_USE_XML)
 
