dcf2e4d4ac7d948f4e6848b4874661a1a8653ca0
Andrew Mikhail <andrewmikhail@ymail.com>
2025-07-28 18:14:20 -0700

    Reworked parameter

--- framework/include/vx_parameter.h
+++ framework/include/vx_parameter.h
@@ -71,6 +71,96 @@ public:
      */
     static vx_bool isValidState(vx_enum state);
 
+    /**
+     * @brief Returns the direction of the parameter
+     *
+     * @return vx_enum The direction of the parameter: VX_INPUT, VX_OUTPUT, or VX_BIDIRECTIONAL
+     * @ingroup group_int_parameter
+     */
+    vx_enum direction() const;
+
+    /**
+     * @brief Returns the index of the parameter
+     *
+     * @return vx_uint32 The index of the parameter
+     * @ingroup group_int_parameter
+     */
+    vx_uint32 idx() const;
+
+    /**
+     * @brief Returns the data type of the parameter
+     *
+     * @return vx_enum The type of the parameter
+     * @ingroup group_int_parameter
+     */
+    vx_enum dataType() const;
+
+    /**
+     * @brief Returns the state of the parameter
+     *
+     * @return vx_enum The state of the parameter: VX_PARAMETER_STATE_REQUIRED or
+     * VX_PARAMETER_STATE_OPTIONAL
+     * @ingroup group_int_parameter
+     */
+    vx_enum state() const;
+
+    /**
+     * @brief Returns the reference referred to by the parameter
+     *
+     * @return vx_reference The reference contained in the parameter
+     * @ingroup group_int_parameter
+     */
+    vx_reference ref() const;
+
+    /**
+     * @brief Returns the meta format contained in the parameter
+     *
+     * @return vx_meta_format The meta format contained in the parameter
+     * @ingroup group_int_parameter
+     */
+    vx_meta_format metaFormat() const;
+
+    /**
+     * @brief Get a parameter by index from a kernel
+     *
+     * @param kernel The kernel to get the parameter from
+     * @param index  The index of the parameter
+     * @return vx_parameter The parameter object, or nullptr if not found
+     * @ingroup group_int_parameter
+     */
+    static vx_parameter getKernelParameterByIndex(vx_kernel kernel, vx_uint32 index);
+
+    /**
+     * @brief Get a parameter by index from a node
+     *
+     * @param node  The node to get the parameter from
+     * @param index The index of the parameter
+     * @return vx_parameter The parameter object, or nullptr if not found
+     * @ingroup group_int_parameter
+     */
+    static vx_parameter getParameterByIndex(vx_node node, vx_uint32 index);
+
+    /**
+     * @brief Set a parameter by index on a node
+     *
+     * @param node   The node to set the parameter on
+     * @param index  The index of the parameter
+     * @param value  The reference to set as the parameter value
+     * @return vx_status VX_SUCCESS on success, error code otherwise
+     * @ingroup group_int_parameter
+     */
+    static vx_status setParameterByIndex(vx_node node, vx_uint32 index, vx_reference value);
+
+    /**
+     * @brief Get a parameter by index from a node
+     *
+     * @param node  The node to get the parameter from
+     * @param index The index of the parameter
+     * @return vx_parameter The parameter object, or nullptr if not found
+     * @ingroup group_int_parameter
+     */
+    vx_status setParameterByReference(vx_reference value);
+
     /*! \brief Index at which this parameter is tracked in both the node references and kernel signatures */
     vx_uint32      index;
     /*! \brief Pointer to the node which this parameter is associated with */
